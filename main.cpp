68094c03549f2a665c7dc37d360771bfd4eaba25
michaldudek <michalpaweldudek@gmail.com>
2023-06-03 16:50:21 +0200

    finished

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ int main() {
 
     powrot:
     wyczyscKonsole();
+
+    cout << green;
     cout<< "[1] wczytaj dane z wejscia konsolowego \n"
         << "[2] wczytaj dane z pliku \n";
+    cout << resetColor;
 
     // wybor opcji wczytania danych
     int wybor;
@@ -20,16 +23,21 @@ int main() {
     if (wybor == 1) {
 
         { // ustawienie tasmy poczatkowej z konsoli
+            cout << green;
             cout << "podaj tasme startowa: " << endl;
+            cout << resetColor;
+
             string tasma;
             cin >> tasma;
             cin.ignore();
             maszyna.ustawTasme (tasma);
         }
 
-        cout << "podawaj w kolejnych liniach reguly przejsc maszyny Turinga" << endl;
+        cout << green;
+        cout << "\npodawaj w kolejnych liniach reguly przejsc maszyny Turinga" << endl;
         cout << "przykladowa regula: q0 . -> q0 0 R" << endl;
         cout << "linia z napisem START zakonczy wczytywanie" << endl;
+        cout << resetColor;
 
         while (true) { // wczytanie kolejnych regul z konsoli
             getline (cin, regula);
@@ -40,7 +48,9 @@ int main() {
 
     }
     else if (wybor == 2) {
+        cout << green;
         cout << "podaj sciezke do pliku z danymi wejsciowymi" << endl;
+        cout << resetColor;
 
         string sciezka;
         cin >> sciezka;
@@ -68,10 +78,17 @@ int main() {
     }
 
     wyczyscKonsole();
+    cout << green;
     cout << "wczytana maszyna:\n" << endl;
-    cout << maszyna;
+    cout << resetColor;
 
+    cout << blue;
+    cout << maszyna;
+    cout << resetColor;
+
+    cout << green;
     cout << "\nczy uruchomic wczytana maszyne?\n" << endl << "[1] tak" << endl << "[2] nie, wczytaj jeszcze raz" << endl << "[3] wyjscie z programu" << endl;
+    cout << resetColor;
     cin >> wybor;
 
     if (wybor == 1) {
@@ -88,7 +105,10 @@ int main() {
         cerr << "niepoprawny wybor" << endl;
     }
 
+    cout << green;
     cout << "[1] wyswietl historie w konsoli" << endl << "[2] zapisz historie do pliku" << endl << "[3] wyjdz z programu" << endl;
+    cout << resetColor;
+
     cin >> wybor;
 
     if (wybor == 1) {
@@ -97,7 +117,9 @@ int main() {
     }
 
     else if (wybor == 2) {
+        cout << green;
         cout << "podaj nazwe pliku: ";
+        cout << resetColor;
         string sciezka;
         cin >> sciezka;
         maszyna.zapiszOutputDoPliku(sciezka);
--- maszynaTuringa.h
+++ maszynaTuringa.h
@@ -8,6 +8,19 @@
 
 using namespace std;
 
+const string red = "\033[31m";
+const string green = "\033[32m";
+const string yellow = "\033[33m";
+const string blue = "\033[34m";
+const string magenta = "\033[35m";
+const string black = "\033[30m";
+const string blackOnWhite = "\033[30;47m";
+const string whiteOnBlack = "\033[37;40m";
+const string backgroundGreen = "\033[42m";
+const string bold = "\033[1m";
+const string resetBold = "\033[22m";
+const string resetColor = "\033[0;0m";
+
 void wyczyscKonsole();
 
 struct RegulaPrzejscia {
